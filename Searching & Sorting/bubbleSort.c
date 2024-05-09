d5f4da69ed0233b22e639d4e374ea09977153cc6
randiPalguna <158301222+randiPalguna@users.noreply.github.com>
2024-05-09 21:39:40 +0700

    Create bubbleSort.c

---
+++ Searching & Sorting/bubbleSort.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+void bubbleSort(int n, int arr[]) {
+  for (int i = 0; i < n - 1; i++) {
+    for (int j = 0; j < n - 1 - i; j++) {
+      if (arr[j] > arr[j + 1]) {
+        int temp = arr[j];
+        arr[j] = arr[j + 1];
+        arr[j + 1] = temp;
+      }
+    }
+  }
+}
+
+int main() {
+  int n;
+  scanf("%d", &n);
+  int arr[n];
+  for (int i = 0; i < n; i++) {
+    scanf("%d", &arr[i]);
+  }
+  bubbleSort(n, arr);
+  for (int i = 0; i < n; i++) {
+    printf("%d ", arr[i]);
+  }
+  return 0;
+}
