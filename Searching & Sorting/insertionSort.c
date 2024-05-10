971c5b8b95d1e3cc56e5e1f7ef26305ab4e50e7d
randiPalguna <158301222+randiPalguna@users.noreply.github.com>
2024-05-10 19:15:25 +0700

    Create insertionSort.c

---
+++ Searching & Sorting/insertionSort.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+void insertionSort(int n, int arr[]) {
+  int pos;
+  for (int i = 0; i < n; i++) {
+    pos = i;
+    while (pos > 0 && (arr[pos] < arr[pos - 1])) {
+      int temp = arr[pos];
+      arr[pos] = arr[pos - 1];
+      arr[pos - 1] = temp;
+      pos--;
+    }
+  }
+}
+
+int main() {
+  int n;
+  scanf("%d", &n);
+  int arr[n];
+  for (int i = 0; i < n; i++) {
+    scanf("%d", &arr[i]);
+  }
+  insertionSort(n, arr);
+  for (int i = 0; i < n; i++) {
+    printf("%d ", arr[i]);
+  }
+  return 0;  
+}
