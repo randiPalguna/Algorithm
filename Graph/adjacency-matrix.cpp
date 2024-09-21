df1400a6f345ad23d7ede228fbe4742201cadc56
Randi Palguna A. <158301222+randiPalguna@users.noreply.github.com>
2024-09-21 18:17:00 +0700

    Update adjacency-matrix.cpp

--- Graph/adjacency-matrix.cpp
+++ Graph/adjacency-matrix.cpp
@@ -11,18 +11,8 @@ struct graph {
       edges.push_back(temp);
     }
   }
-
-  void check() {
-    cout << edges.size() << '\n';
-    for (int i = 0; i < (int)edges.size(); i++) {
-      for (int j = 0; j < (int)edges.size(); j++) {
-        cout << edges[i][j] << " ";
-      }
-      cout << '\n';
-    }
-  }
   
-  // If unweighted graph
+  // addEdge if unweighted graph
   void addEdge(int src, int dst, bool undirected) {
     edges[src][dst] = 1;
     if (undirected) {
@@ -30,11 +20,28 @@ struct graph {
     }
   }
 
-  // If weighted graph
+  // addEdgeW if weighted graph
   void addEdgeW(int src, int dst, int weigth, bool undirected) {
     edges[src][dst] = weigth;
     if (undirected) {
       edges[dst][src] = weigth;
+    }
+  }
+
+  // Weighted or Unweighted can use the same function to delete edge
+  void delEdge(int src, int dst, bool undirected) {
+    edges[src][dst] = 0;
+    if (undirected) {
+      edges[dst][src] = 0;
+    }
+  }
+
+  void printList() {
+    for (int i = 0; i < (int)edges.size(); i++) {
+      for (int j = 0; j < (int)edges.size(); j++) {
+        cout << edges[i][j] << " ";
+      }
+      cout << '\n';
     }
   }
 };
@@ -42,10 +49,10 @@ struct graph {
 int main() {
   graph g;
   g.init(5);
-  g.check();
   g.addEdgeW(1, 4, 10, 1);
   g.addEdgeW(1, 2, 69, 1);
   g.addEdgeW(2, 3, 25, 1);
-  g.check();
+  g.delEdge(2, 3, 1);
+  g.printList();
   return 0;
 }
